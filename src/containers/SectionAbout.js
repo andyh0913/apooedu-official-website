87fdf49a65d49656ccdd639b9f4cc665891e5123
andyh0913 <andyh0913@gmail.com>
2020-09-19 10:35:40 +0800

    add several sketches

--- src/containers/SectionAbout.js
+++ src/containers/SectionAbout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
+import Title from '../components/Title';
 import '../App.css';
 
 function SectionAbout(){
     return (
         <section id="section-about" className="vh-100">
-            <Container className="h-100">
-                <Row>
-                    <Col className="h-100">
+            <Container>
+                <Title text="關於我們"></Title>
+                <Row className="justify-content-center">
+                    <Col md={8} className="about">
                     </Col>
                 </Row>
             </Container>
--- src/containers/SectionLessons.js
+++ src/containers/SectionLessons.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
+import Lesson from '../components/Lesson';
+import Title from '../components/Title';
 import '../App.css';
 
 function SectionLessons(){
     return (
         <section id="section-lessons" className="vh-100">
-            <Container>
-                
+            <Container className="justify-content-center">
+                <Title text="課程介紹"></Title>
+                <Row className="justify-content-center">
+                    <Lesson></Lesson>
+                    <Lesson></Lesson>
+                    <Lesson></Lesson>
+                </Row>
             </Container>
         </section>
     )
